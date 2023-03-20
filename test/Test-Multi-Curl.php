fed7fac8e440c2f219cfc2b557bf51fc259dc032
Hozoy <zrincet@gmail.com>
2023-03-20 19:44:01 +0800

    fix for test

--- test/Test-Multi-Curl.php
+++ test/Test-Multi-Curl.php
@@ -9,30 +9,21 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $startTime = microtime(true);
 $arkMultiCurl = new ArkMultiCurl();
 $x = new ArkCurl();
-$arkCurl = $x->prepareToRequestURL('GET', 'http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/33/3301.html')
+$arkCurl = $x->prepareToRequestURL('GET', 'https://sinri.cc/frontend/index.html')
     ->setAcceptEncoding();
 $arkMultiCurl->addCurl($arkCurl);
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
-$arkMultiCurl->addCurl((new ArkCurl())->prepareToRequestURL('POST', 'https://sinri.cc/frontend/index.html')
-    ->setAcceptEncoding());
+
+$curlList = [];
+for ($i = 0; $i < 10; $i++) {
+    $curlList[] = (new ArkCurl())->prepareToRequestURL('POST', 'https://www.php.net/manual/zh/function.curl-multi-exec.php')
+        ->setAcceptEncoding();
+}
+
+$arkMultiCurl->addCurlList($curlList);
+
 $resultList = $arkMultiCurl->execute();
 
 var_dump($resultList['resultList']);
 
 $endTime = microtime(true);
-echo sprintf("use time: %.3f s".PHP_EOL, $endTime - $startTime);
+echo sprintf("use time: %.3f s" . PHP_EOL, $endTime - $startTime);
